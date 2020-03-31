c89b6336067cec04b8b2d342f5e08fd878320cea
Alix FDM <boujour@alixfdm.fr>
2020-03-31 08:57:47 -0300

    add .wid class

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 function DetectTallImg() {
   const galleryImages = document.querySelectorAll(".gallery img");
   galleryImages.forEach(image => {
-    if (image.height >= image.width * 1.3) {
-      console.log("hell ya !", image.height);
+    if (image.height > image.width * 1.3) {
       image.classList.add("tall");
+    } else if (image.height < image.width * 1.3) {
+      image.classList.add("wide");
     }
   });
 }
