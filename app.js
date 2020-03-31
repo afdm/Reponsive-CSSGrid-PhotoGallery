9bcf53d2899543a139e2a8f6b35fa419067110d7
Alix FDM <boujour@alixfdm.fr>
2020-03-31 01:20:52 -0300

    ajust the tall detection

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 function DetectTallImg() {
   const galleryImages = document.querySelectorAll(".gallery img");
   galleryImages.forEach(image => {
-    if (image.height >= 0.8 * (1 + image.width)) {
+    if (image.height >= 0.8 * image.width) {
       console.log("hell ya !", image.height);
       image.classList.add("tall");
     }
