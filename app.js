6877b9d3bd9cd0e6b98750860e5d73781c33d818
Alix FDM <boujour@alixfdm.fr>
2020-03-31 08:32:56 -0300

    width coef

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 function DetectTallImg() {
   const galleryImages = document.querySelectorAll(".gallery img");
   galleryImages.forEach(image => {
-    if (image.height >= 0.8 * image.width) {
+    if (image.height >= image.width * 1.3) {
       console.log("hell ya !", image.height);
       image.classList.add("tall");
     }
